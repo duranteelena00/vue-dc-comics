e67c1d5552ecf929fdd02c7feaeb04ffc6aa4dbb
durante.elena00 <durante.elena00@gmail.com>
2021-09-08 18:02:38 +0200

    implement products section and fix the dynamic links in the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,16 +8,6 @@
             link.text
           }}</a>
         </li>
-        <!-- <li><a href="#">Characters</a></li>
-          <li><a href="#" class="active">Comics</a></li>
-          <li><a href="#">Movies</a></li>
-          <li><a href="#">TV</a></li>
-          <li><a href="#">Games</a></li>
-          <li><a href="#">Collectibles</a></li>
-          <li><a href="#">Videos</a></li>
-          <li><a href="#">Fans</a></li>
-          <li><a href="#">News</a></li>
-          <li><a href="#">Shop</a></li> -->
       </ul>
     </nav>
   </header>
@@ -27,9 +17,61 @@
 export default {
   name: "Header",
   data() {
-    return {};
+    return {
+      links: [
+        {
+          text: "Characters",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Comics",
+          url: "#",
+          current: true,
+        },
+        {
+          text: "Movies",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "TV",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Games",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Collectibles",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Videos",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Fans",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "News",
+          url: "#",
+          current: false,
+        },
+        {
+          text: "Shop",
+          url: "#",
+          current: false,
+        },
+      ],
+    };
   },
-  props: "links",
 };
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,9 +2,14 @@
   <main>
     <section id="products">
       <div class="container">
+        <button>CURRENT SERIES</button>
         <ul>
-          <li></li>
+          <li v-for="(item, index) in items" :key="index">
+          <img :src="item.thumb">
+          <p>{{item.series}}</p>
+        </li>
         </ul>
+        <button>LOAD MORE</button>
       </div>
     </section>
     <section id="links">
@@ -49,22 +54,170 @@
 <script>
 export default {
   name: "Main",
+  data() {
+    return {
+      items: [
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+          price: "$19.99",
+          series: "Action Comics",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+          price: "$3.99",
+          series: "American Vampire 1976",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+          price: "$16.99",
+          series: "Aquaman",
+          type: "graphic novel",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+          price: "$2.99",
+          series: "Batgirl",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          price: "$3.99",
+          series: "Batman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+          price: "$2.99",
+          series: "Batman Beyond",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+          price: "$3.99",
+          series: "Batman/Superman",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+          price: "$4.99",
+          series: "Batman/Superman Annual",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+          price: "$5.99",
+          series: "Batman: The Joker War Zone",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+          price: "$6.99",
+          series: "Batman: Three Jokers",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+          price: "$4.99",
+          series: "Batman: White Knight Presents: Harley Quinn",
+          type: "comic book",
+        },
+        {
+          thumb:
+            "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+          price: "$16.99",
+          series: "Catwoman",
+          type: "graphic novel",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style scoped>
 section#products {
-  height: 350px;
   background: #202020;
+  padding: 15px 0 20px 0;
+}
+  #products .container {
+    position: relative;
+     display: flex;
+  flex-direction: column;
+  }
+
+#products .container,
+#products ul {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  flex-wrap: wrap;
 }
 
-  #products ul,
   #links ul {
     display: flex;
     justify-content: center;
     align-items: center;
+    vertical-align: top;
   }
-  
+
+  #products li {
+    flex-basis: calc(100% / 6.5);
+    padding: 15px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  #products li img {
+    width: 100%;
+    height: calc(900px / 6.5);
+    object-fit: cover;
+    object-position: top;
+  }
+
+  #products li p {
+    padding-top: 10px;
+    font-size: 11px;
+    color: white;
+    text-transform: uppercase;
+  }
+
+  #products button {
+    padding: 7.5px 12.5px;
+    background-color: #0c7cec;
+    color: white;
+    border: none;
+    font-weight: bold;
+  }
+
+  #products button:first-of-type {
+    position: absolute;
+    top: -30px;
+    left: 0;
+    font-size: 14px;
+  }
+
+  #products button:nth-of-type(2) {
+    margin-top: 10px;
+    padding: 5px 30px;
+    font-size: 10px;
+    align-self: center;
+  }
+
 section#links {
   background: dodgerblue;
   padding: 40px 0;
@@ -72,7 +225,7 @@ section#links {
   z-index: 2;
 }
 
-section#links .container {
+#links .container {
   display: flex;
   justify-content: center;
   align-items: center;
